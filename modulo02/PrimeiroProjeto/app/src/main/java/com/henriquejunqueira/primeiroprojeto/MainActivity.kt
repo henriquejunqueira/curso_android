caa99f0c2e8972d19fb6a551db2ff4e585f0ad39
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-12 01:22:44 -0300

    Adding button click event

--- modulo02/PrimeiroProjeto/app/src/main/java/com/henriquejunqueira/primeiroprojeto/MainActivity.kt
+++ modulo02/PrimeiroProjeto/app/src/main/java/com/henriquejunqueira/primeiroprojeto/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.henriquejunqueira.primeiroprojeto
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun clique(view: View){
+        println("foi clicado!")
+    }
+
 }
