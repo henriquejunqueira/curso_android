3d75bb78deb88fa6954a9cabc036643418401ac9
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-12 15:54:49 -0300

    Creating a simple lottery app with random numbers

--- modulo02/PrimeiroProjeto/app/src/main/java/com/henriquejunqueira/primeiroprojeto/MainActivity.kt
+++ modulo02/PrimeiroProjeto/app/src/main/java/com/henriquejunqueira/primeiroprojeto/MainActivity.kt
@@ -2,10 +2,12 @@ package com.henriquejunqueira.primeiroprojeto
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +21,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun clique(view: View){
-        println("foi clicado!")
+    // função que será executada quando for clicado/pressionado o botão de sorteio
+    fun sortear(view: View){
+
+        // println("foi clicado!")
+
+        // encontra um item de interface por ID e salva na variável "textoResultado"
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+
+        // gera números de 0 a 10, o 11 não entra
+        val numeroSorteado = Random().nextInt(11)
+
+        // seta o valor do número sorteado na variável textoResultado que vai aparecer na tela
+        textoResultado.setText("número gerado $numeroSorteado")
+
     }
 
 }
