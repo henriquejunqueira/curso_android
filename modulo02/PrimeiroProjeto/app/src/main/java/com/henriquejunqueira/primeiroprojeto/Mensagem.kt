a3c450f066189facc6659af6d18e0704f26b8e4d
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-12 16:09:50 -0300

    Interoperability between Java and Kotlin

---
+++ modulo02/PrimeiroProjeto/app/src/main/java/com/henriquejunqueira/primeiroprojeto/Mensagem.kt
@@ -0,0 +1,16 @@
+package com.henriquejunqueira.primeiroprojeto
+
+// Interoperabilidade -> utilizar código java no kotlin e vice versa
+class Mensagem {
+
+    //fun enviar(){
+    fun enviarNovaMensagem(){
+        //val usuario = Usuario()
+        //usuario.logar()
+        //usuario.autenticar()
+
+        // simplificado
+        Usuario().autenticar()
+    }
+
+}
---
+++ modulo02/PrimeiroProjeto/app/src/main/java/com/henriquejunqueira/primeiroprojeto/Usuario.java
@@ -0,0 +1,9 @@
+package com.henriquejunqueira.primeiroprojeto;
+
+public class Usuario {
+    //public void logar(){
+    public void autenticar(){
+        //new Mensagem().enviar();
+        new Mensagem().enviarNovaMensagem();
+    }
+}
